fb700736ca6c653d8cb252933599d9e5f443b103
Efe Karakanlı <102619516+actuallyzefe@users.noreply.github.com>
2023-03-19 02:05:12 +0300

    refactor: removed getters - passthrough Methods

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -1,4 +1,4 @@
-import { AxiosPromise, AxiosResponse } from 'axios';
+import { AxiosPromise, AxiosResponse } from "axios";
 
 interface ModelAttributes<T> {
   set(value: T): void;
@@ -27,47 +27,37 @@ export class Model<T extends HasId> {
     private sync: Sync<T>
   ) {}
 
-  get on() {
-    return this.events.on;
-  }
+  on = this.events.on;
 
-  get trigger() {
-    return this.events.trigger;
-  }
+  trigger = this.events.trigger;
 
-  get get() {
-    return this.attributes.get;
-  }
+  get = this.attributes.get;
 
   set(update: T): void {
     this.attributes.set(update);
-    this.events.trigger('change');
+    this.events.trigger("change");
   }
 
   fetch(): void {
-    const id = this.get('id');
+    const id = this.get("id");
 
-    if (typeof id !== 'number') {
-      throw new Error('Cannot fetch without an id');
+    if (typeof id !== "number") {
+      throw new Error("Cannot fetch without an id");
     }
 
-    this.sync.fetch(id).then(
-      (response: AxiosResponse): void => {
-        this.set(response.data);
-      }
-    );
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
   }
 
   save(): void {
     this.sync
       .save(this.attributes.getAll())
-      .then(
-        (response: AxiosResponse): void => {
-          this.trigger('save');
-        }
-      )
+      .then((response: AxiosResponse): void => {
+        this.trigger("save");
+      })
       .catch(() => {
-        this.trigger('error');
+        this.trigger("error");
       });
   }
 }
